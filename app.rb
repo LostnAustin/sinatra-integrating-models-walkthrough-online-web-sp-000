6f7475da8f1652f93d87c84291b7eff60d7000e0
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-08 07:08:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     @count_of_vowels = @analyzed_text.count_of_vowels
     @count_of_consonants = @analyzed_text.count_of_consonants
     @most_used_letter = @analyzed_text.most_used_letter.first[0].upcase
-    @most_used_letter_count = @analyzed_text.most_used_letter.first[1]
+    @most_used_letter_count = @analyzed_text.most_used_letter.first[1].to_i
 # binding.pry
     erb :results
   end
